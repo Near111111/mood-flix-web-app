798b3b5c93b7bb07357915575208592947024f3c
Near111111 <Jomarmerej@gmail.com>
2025-04-19 17:46:18 +0800

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: 5173, // You can change this port if needed
+  },
 });
